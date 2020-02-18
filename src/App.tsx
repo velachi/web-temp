13b04831d1bb0d9338b8e145f7a956e9d7c4cad1
Dennis Kievits <denkievits@gmail.com>
2020-02-18 22:24:16 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1>Velachi</h1>
+        <small className="App-goodbye-message">Currently under construction. Please visit us later.</small>
+      </header>
+    </div>
+  );
+}
+
+export default App;
