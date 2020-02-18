44ddc4bc10c198e109d99583ebf688884674b31c
Dennis Kievits <denkievits@gmail.com>
2020-02-18 22:29:25 +0100

    Updates

---
+++ precache-manifest.ae9564ac2d02766b5a4a5cc5f2925520.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d36387b07d82168cebcfe7bb6b55797",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b1144b00bff92cf4ad7",
+    "url": "/static/css/main.81b20fd2.chunk.css"
+  },
+  {
+    "revision": "b503a5c1b2794e9dc4fa",
+    "url": "/static/js/2.533804f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.533804f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1144b00bff92cf4ad7",
+    "url": "/static/js/main.9a38720b.chunk.js"
+  },
+  {
+    "revision": "2ba5a17d58e00f8b4184",
+    "url": "/static/js/runtime-main.ebc1224c.js"
+  },
+  {
+    "revision": "c1128a55f741b23648568490a061b41b",
+    "url": "/static/media/logo.c1128a55.svg"
+  }
+]);
